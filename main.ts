3a66487c66632e46420cc44bf4423c50e0ae65d1
eduardani84 <eduardani84@gmail.com>
2025-05-30 21:31:14 +0000

    [add] - connect to api

--- main.ts
+++ main.ts
@@ -15,6 +15,27 @@ server.tool(
         city: z.string().describe('City name'),
     },
     async ({ city }) => {
+        const response = await fetch(`https://geocoding-api.open-meteo.com/v1/search?name=${city}&count=10&language=en&format=json`)
+        const data = await response.json()
+
+        if (data.length === 0)
+        {
+            return {
+                content: [
+                    {
+                        type: 'text',
+                        text: `No se encontro informacion para la coidad ${city}`
+                     }
+                ]
+            }
+        }
+
+        const { latitude, longitude} = data[0]
+
+        const weatherResponse = await fetch(`https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude }&hourly=temperature_2m&current=temperature_2m,precipitation,is_day,rain&forecast_days=1`)
+
+        const weatherData = await weatherResponse.json()
+
         return {
             content: [
                 {
